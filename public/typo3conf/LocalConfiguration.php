748d0420d18c0e09f4d9c91ebeb807066d726a40
Erik Sommer <erik.sommer@slub-dresden.de>
2022-09-06 21:20:45 +0200

    Initial commit

---
+++ public/typo3conf/LocalConfiguration.php
@@ -0,0 +1,145 @@
+<?php
+return [
+    'BE' => [
+        'debug' => true,
+        'explicitADmode' => 'explicitAllow',
+        'installToolPassword' => '$argon2i$v=19$m=65536,t=16,p=1$RXIwTm16TWhMVnRoczd3RQ$NkBA70zXbrPTIrv9ik2Ev0cg8TEnaX7wc6FNXv+Z1FQ',
+        'loginSecurityLevel' => 'normal',
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
+            'options' => [],
+        ],
+    ],
+    'DB' => [
+        'Connections' => [
+            'Default' => [
+                'charset' => 'utf8mb4',
+                'dbname' => 'db',
+                'driver' => 'mysqli',
+                'host' => 'db',
+                'password' => 'db',
+                'port' => 3306,
+                'tableoptions' => [
+                    'charset' => 'utf8mb4',
+                    'collate' => 'utf8mb4_unicode_ci',
+                ],
+                'user' => 'db',
+            ],
+        ],
+    ],
+    'EXT' => [
+        'extConf' => [
+            'backend' => 'a:6:{s:14:"backendFavicon";s:0:"";s:11:"backendLogo";s:0:"";s:20:"loginBackgroundImage";s:0:"";s:13:"loginFootnote";s:0:"";s:19:"loginHighlightColor";s:0:"";s:9:"loginLogo";s:0:"";}',
+            'dpf' => 'a:10:{s:10:"fedoraHost";s:6:"fcrepo";s:10:"fedoraUser";s:4:"user";s:14:"fedoraPassword";s:8:"password";s:14:"fedoraEndpoint";s:6:"fcrepo";s:19:"fedoraRootContainer";s:4:"rest";s:25:"fedoraCollectionNamespace";s:10:"qucosa:all";s:17:"elasticSearchHost";s:25:"ddev-qucosa-elasticsearch";s:17:"elasticSearchPort";s:4:"9200";s:15:"uploadDirectory";s:14:"uploads/tx_dpf";s:12:"uploadDomain";s:0:"";}',
+            'extensionmanager' => 'a:2:{s:21:"automaticInstallation";s:1:"1";s:11:"offlineMode";s:1:"0";}',
+            'gridelements' => 'a:6:{s:20:"additionalStylesheet";s:0:"";s:38:"disableAutomaticUnusedColumnCorrection";s:1:"0";s:25:"disableCopyFromPageButton";s:1:"0";s:19:"disableDragInWizard";s:1:"0";s:19:"nestingInListModule";s:1:"0";s:26:"overlayShortcutTranslation";s:1:"0";}',
+            'scheduler' => 'a:2:{s:11:"maxLifetime";s:4:"1440";s:15:"showSampleTasks";s:1:"1";}',
+            'vhs' => 'a:1:{s:20:"disableAssetHandling";s:1:"0";}',
+        ],
+    ],
+    'EXTCONF' => [
+        'lang' => [
+            'availableLanguages' => [
+                'de',
+            ],
+        ],
+    ],
+    'EXTENSIONS' => [
+        'backend' => [
+            'backendFavicon' => '',
+            'backendLogo' => '',
+            'loginBackgroundImage' => '',
+            'loginFootnote' => '',
+            'loginHighlightColor' => '',
+            'loginLogo' => '',
+        ],
+        'dpf' => [
+            'elasticSearchHost' => 'ddev-qucosa-elasticsearch',
+            'elasticSearchPort' => '9200',
+            'fedoraCollectionNamespace' => 'qucosa:all',
+            'fedoraEndpoint' => 'fcrepo',
+            'fedoraHost' => 'fcrepo',
+            'fedoraPassword' => 'password',
+            'fedoraRootContainer' => 'rest',
+            'fedoraUser' => 'user',
+            'uploadDirectory' => 'uploads/tx_dpf',
+            'uploadDomain' => '',
+        ],
+        'extensionmanager' => [
+            'automaticInstallation' => '1',
+            'offlineMode' => '0',
+        ],
+        'gridelements' => [
+            'additionalStylesheet' => '',
+            'disableAutomaticUnusedColumnCorrection' => '0',
+            'disableCopyFromPageButton' => '0',
+            'disableDragInWizard' => '0',
+            'nestingInListModule' => '0',
+            'overlayShortcutTranslation' => '0',
+        ],
+        'scheduler' => [
+            'maxLifetime' => '1440',
+            'showSampleTasks' => '1',
+        ],
+        'vhs' => [
+            'disableAssetHandling' => '0',
+        ],
+    ],
+    'FE' => [
+        'debug' => true,
+        'loginSecurityLevel' => 'normal',
+        'pageNotFoundOnCHashError' => false,
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
+            'options' => [],
+        ],
+    ],
+    'GFX' => [
+        'processor' => 'ImageMagick',
+        'processor_allowTemporaryMasksAsPng' => false,
+        'processor_colorspace' => 'RGB',
+        'processor_effects' => false,
+        'processor_enabled' => true,
+        'processor_path' => '/usr/bin/',
+        'processor_path_lzw' => '/usr/bin/',
+    ],
+    'LOG' => [
+        'TYPO3' => [
+            'CMS' => [
+                'deprecations' => [
+                    'writerConfiguration' => [
+                        5 => [
+                            'TYPO3\CMS\Core\Log\Writer\FileWriter' => [
+                                'disabled' => false,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'MAIL' => [
+        'transport' => 'smtp',
+        'transport_sendmail_command' => '/usr/local/bin/mailhog sendmail test@example.org --smtp-addr 127.0.0.1:1025',
+        'transport_smtp_encrypt' => '',
+        'transport_smtp_password' => '',
+        'transport_smtp_server' => 'localhost:1025',
+        'transport_smtp_username' => '',
+    ],
+    'SYS' => [
+        'devIPmask' => '*',
+        'displayErrors' => 1,
+        'encryptionKey' => 'd7afe50310ca126e7f32d62c8870cba5198e2f565c6fedf524a7ec7aede663669a11811f44140f78c73f9a721a8ec44a',
+        'exceptionalErrors' => 12290,
+        'features' => [
+            'newTranslationServer' => true,
+            'unifiedPageTranslationHandling' => false,
+        ],
+        'sitename' => 'EXT:Qucosa DDEV',
+        'systemLogLevel' => 0,
+        'systemMaintainers' => [
+            1,
+        ],
+        'trustedHostsPattern' => '.*.*',
+    ],
+];
